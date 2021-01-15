7a23d31124296df560b1b8eeb97f0994bc2393fb
ArtemKhairov <artemxairoff@yandex.ru>
2021-01-15 19:17:08 +0700

    третий комит
    Рефакторинг state.js
    Добавлены reducer(ы)
    -dialogs-reducer
    -profile-reducer
    -sidebar-reducer

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,10 +1,6 @@
-// типы экшенов
-const ADD_POST = "ADD-POST";
-const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
-
-// для отправки и изменения сообщений
-const UPDATE_NEW_MESSAGE_BODY = "UPDATE-NEW-MESSAGE-BODY";
-const SEND_MESSAGE = "SEND-MESSAGE";
+import profileReducer from './profile-reducer';
+import dialogsReducer from './dialogs-reducer';
+import sidebarReducer from './sidebar-reducer';
 
 let store = {
   _state: {
@@ -57,56 +53,16 @@ let store = {
   // в зависимости от типа
   // выполняет разные функции в store
   dispatch(action) {
-    if (action.type === ADD_POST) {
-      // debugger;
-      let newPost = {
-        id: 5,
-        message: this._state.profilePage.newPostText,
-        likesCount: 0,
-      };
-      this._state.profilePage.posts.push(newPost);
-      this._state.profilePage.newPostText = "";
-      this._callSubscriber(this._state);
-    } else if (action.type === UPDATE_NEW_POST_TEXT) {
-      // debugger;
-      this._state.profilePage.newPostText = action.newText;
-      this._callSubscriber(this._state);
-    } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-      this._state.dialogsPage.newMessageBody = action.body;
-      this._callSubscriber(this._state);
-    } else if (action.type === SEND_MESSAGE) {
-      let body = this._state.dialogsPage.newMessageBody;
-      this._state.dialogsPage.newMessageBody = "";
-      this._state.dialogsPage.messages.push({ id: 7, message: body });
-      this._callSubscriber(this._state);
-    }
+    
+    // передача экшена и нужной части стейта в редьюсер
+    // редьюсер обрабатывает и возвращает новый стейт
+    this._state.profilePage = profileReducer(this._state.profilePage, action);
+    this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
+    this._state.sidebar = sidebarReducer(this._state.sidebar, action);
+
+    this._callSubscriber(this._state);
+
   },
-};
-
-export const addPostActionCreator = () => {
-  return {
-    type: ADD_POST,
-  };
-};
-
-export const updateNewPostTextActionCreator = (text) => {
-  return {
-    type: UPDATE_NEW_POST_TEXT,
-    newText: text,
-  };
-};
-
-export const sendMessageCreator = () => {
-  return {
-    type: SEND_MESSAGE,
-  };
-};
-
-export const updateNewMessageBodyCreator = (body) => {
-  return {
-    type: UPDATE_NEW_MESSAGE_BODY,
-    body: body,
-  };
 };
 
 export default store;
