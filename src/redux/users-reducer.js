dc8879c5b6a1043c8f49eaa7af5e5862eb949e42
ArtemKhairov <artemxairoff@yandex.ru>
2021-01-27 21:04:32 +0700

    UsersPage update
    Добавлены стили и разметка Users
    Начальный стейт убран в Users

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,29 +3,7 @@ const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
 
 let initialState = {
-  users: [
-    {
-      id: 1,
-      followed: true,
-      fullName: "Artur",
-      status: "want to drink",
-      location: { city: "New-York", country: "USA" },
-    },
-    {
-      id: 2,
-      followed: false,
-      fullName: "Donald",
-      status: "I'm done",
-      location: { city: "Toronto", country: "Canada" },
-    },
-    {
-      id: 3,
-      followed: true,
-      fullName: "Artem",
-      status: "Hard working",
-      location: { city: "Krasnoyarsk", country: "Russia" },
-    },
-  ],
+  users: [],
 };
 
 const usersReducer = (state = initialState, action) => {
