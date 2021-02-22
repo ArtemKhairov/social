d43e893a646b76cf1bf63a932fc59946b47dbce3
ArtemKhairov <artemxairoff@yandex.ru>
2021-02-22 21:34:45 +0700

    add try /catch

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -129,9 +129,13 @@ export const getStatus = (userId) => async (dispatch) => {
 };
 
 export const updateStatus = (status) => async (dispatch) => {
-  let response = await profileAPI.updateStatus(status);
-  if (response.data.resultCode === 0) {
-    dispatch(setStatus(status));
+  try {
+    let response = await profileAPI.updateStatus(status);
+    if (response.data.resultCode === 0) {
+      dispatch(setStatus(status));
+    }
+  } catch (error) {
+    //
   }
 };
 
